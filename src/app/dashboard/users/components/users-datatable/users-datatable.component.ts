628ef8f2f0960f164c549de9b5f35922664a85a8
Matias Gomez <matias.gomez@valkimia.com>
2021-03-14 14:08:19 -0300

    Added ngx datatable

--- src/app/dashboard/users/components/users-datatable/users-datatable.component.ts
+++ src/app/dashboard/users/components/users-datatable/users-datatable.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./users-datatable.component.scss"],
 })
 export class UsersDatatableComponent implements OnInit {
+  rows = [
+    {
+      firstName: "Austin",
+      lastName: "Male",
+      username: "Swimlane",
+      email: "test@test.com",
+      role: "admin",
+      birthday: "05/05/97",
+    },
+  ];
+
+  columns = [
+    { name: "First name" },
+    { name: "Last name" },
+    { name: "Username" },
+    { name: "Email" },
+    { name: "Role" },
+    { name: "Birthday" },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
